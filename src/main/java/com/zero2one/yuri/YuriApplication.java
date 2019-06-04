21377db5ded1018b550cbbe79b4372fc46bd3322
Yuri Winche Achermann <yuri_wa@hotmail.com>
2019-06-04 19:44:59 -0300

    first commit

---
+++ src/main/java/com/zero2one/yuri/YuriApplication.java
@@ -0,0 +1,13 @@
+package com.zero2one.yuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero2one/yuri/YuriApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zero2one.yuri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YuriApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
